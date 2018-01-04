8f61b68e05a466288793e7deb78f8c7ac461e673
Ruslan <bakirovruslanr@gmail.com>
2018-01-05 01:11:46 +0300

    add login page

--- src/clj/twitter_impl/routes/login.clj
+++ src/clj/twitter_impl/routes/login.clj
@@ -1,15 +1,41 @@
 (ns twitter-impl.routes.login
     (:require [hiccup.page :refer [include-js include-css html5]]
               [compojure.response :refer [render]]
-              [ring.util.response :refer [redirect]]))
+              [ring.util.response :refer [redirect]]
+
+              [config.core :refer [env]]))
 
 
 (def authdata
   {:admin "admin"
    :test "test"})
 
+(defn head []
+  [:head
+   [:meta {:charset "utf-8"}]
+   [:meta {:name "viewport"
+           :content "width=device-width, initial-scale=1"}]
+   (include-css (if (env :dev) "/css/login-page.css" "/css/site.min.css"))])
+
+(defn root-login [error]
+  [:div#root-login
+   (when (not= error nil) (error))
+   [:h1 {:class "title"}"Login page"]
+   [:form {:method "post"}
+    [:label
+     [:input {:name "username"}]
+     [:span "Login"]]
+    [:label
+     [:input {:name "password"}]
+     [:span "Password"]]
+    [:button {:type "submit"} "Start"]]])
+
+(defn error-login []
+    [:div {:class "error"} "Login or password incorrect"])
+
+
 (defn login [req]
-    (let [content (html5 [:body "Login"])]
+    (let [content (html5 (head) [:body (root-login nil)])]
       (render content req)))
 
 (defn login-authenticate [req]
@@ -22,5 +48,5 @@
             updated-session (assoc session :identity (keyword username))]
         (-> (redirect next-url)
           (assoc :session updated-session)))
-      (let [content (html5 [:body "login-false"])]
+      (let [content (html5 (head) [:body (root-login error-login)])]
         (render content req)))))
